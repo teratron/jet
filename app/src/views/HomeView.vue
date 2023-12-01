855390fb6bfad2ecc080ca78f123ed3d025d28ea
Oleg Alexandrov <alexandrovoleg.ru@gmail.com>
2023-12-01 19:30:06 +0200

    Bases vue

--- app/src/views/HomeView.vue
+++ app/src/views/HomeView.vue
@@ -1,22 +1,7 @@
 <script lang="ts" setup>
-import {ref} from 'vue'
-
-const groceryList = ref([
-    {id: 0, text: 'Vegetables'},
-    {id: 1, text: 'Cheese'},
-    {id: 2, text: 'Whatever else humans are supposed to eat'}
-])
 </script>
 
 <template>
     <h1>Jettix</h1>
     <p>HTML, CSS, SCSS, JavaScript Framework</p>
-    <ol>
-        <li
-            v-for="item in groceryList"
-            :key="item.id"
-        >
-            {{ item.text }}
-        </li>
-    </ol>
 </template>
