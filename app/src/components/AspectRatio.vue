1cbc0a89fda09502ac65a91618313d7af7c24263
Oleg Alexandrov <alexandrovoleg.ru@gmail.com>
2023-12-07 15:27:58 +0200

    AspectRatio.vue

--- app/src/components/AspectRatio.vue
+++ app/src/components/AspectRatio.vue
@@ -1,5 +1,7 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+//import Empty1x1 from 'lib/media/empty_1x1.svg'
+</script>
 
 <template>
-
+    <img alt="" src="Empty1x1"/>
 </template>
